5e4a87a23b038cb2a4eb401d9339bb21dc7723a2
AlinaBuzhynskaya <alinabuzhynskaya@gmail.com>
2017-12-03 20:36:44 +0300

    played with pg connection

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var pg = require('pg');
-var pool = new pg.Pool();
+var pool = new pg.Pool({
+  database: 'todo',
+  user: 'postgres',
+  password: 1234,
+  //connectionString: process.env.DATABASE_URL
+});
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+//app.set('view engine', 'html');
 app.use(bodyParser.json());
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -15,19 +21,20 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 app.get('/', function(request, response) {
-    response.render('dist/index');
+    response.render('dist/index.html');
 });
-
-// process.env.DATABASE_URL
 
 app.get('/db', function (request, response) {
   pool.connect(function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
+      if (err) {
+        console.error(err); response.send("Error " + err);
+      } else {
+        //response.render('dist/index', {results: result.rows} ); 
+        //response.render('index.html');
+        response.sendFile('index.html', {root: distDir});
+      }
       done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('dist/index', {results: result.rows} ); }
     });
   });
 });
