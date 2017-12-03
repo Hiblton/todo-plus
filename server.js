f6c8abc5bdc3e7b4b97280dd6cdc3c0990bfec9e
AlinaBuzhynskaya <alinabuzhynskaya@gmail.com>
2017-12-03 14:10:07 +0300

    fixed server.js file

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var bodyParser = require("body-parser");
 var pg = require('pg');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
