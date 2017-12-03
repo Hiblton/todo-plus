6525d26b1c8f9fc11b51cbdb7e79f849f1750216
AlinaBuzhynskaya <alinabuzhynskaya@gmail.com>
2017-12-03 15:34:25 +0300

    reverted pg connection

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var pg = require('pg');
-var pool = new pg.Pool();
-// var pool = new pg.Pool({
-//   user: 'root',
-//   database: 'todo',
-//   password: '1234'
-// });
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -24,16 +18,13 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-  pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
+      done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
        { response.render('dist/index', {results: result.rows} ); }
     });
-
-    done();
   });
 });
-
-//pool.end();
