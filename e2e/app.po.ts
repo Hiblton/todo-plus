b83baa0e59019151e56150d96a9abb203caba714
slava.selivonchyk <slava.selivonchyk@gmail.com>
2018-01-30 16:32:20 +0300

    fixed e2e tests

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  seeButton() {
+    return element(by.css('button'));
   }
 }
