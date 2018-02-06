08e11c71a3f185212f83ba5ce6e13d250d23e2ac
slava.selivonchyk <slava.selivonchyk@gmail.com>
2018-02-06 17:29:03 +0300

    worked on daysline component

--- src/app/components/daysline/daysline.component.ts
+++ src/app/components/daysline/daysline.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-daysline',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayslineComponent implements OnInit {
 
-  constructor() { }
+  daysList = [];
+  DAYS_FROM = -4;
+  DAYS_TO = 4;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.generateDaysLine();
+  }
+
+  generateDaysLine(date = moment.now()) {
+    const list = [];
+    for (let i = this.DAYS_FROM; i <= this.DAYS_TO; i++) {
+      const dateObj = this.getDateFormat(moment(date).add(i, 'd'));
+      list.push({...dateObj, active: i === 0});
+    }
+    this.daysList = list;
+  }
+
+  getDateFormat(date) {
+    return {
+      fullDate: moment(date).format('MMM Do, YYYY'),
+      day: moment(date).format('DD'),
+      dayOfWeek: moment(date).format('dddd'),
+      shortDayOfWeek: moment(date).format('ddd')
+    };
+  }
+
+  selectDay(i) {
+    this.daysList.map((item, index) => {
+      if (index === i) {
+        this.loadTaskListByDate(item);
+      }
+      return item.active = index === i;
+    });
+  }
+
+  loadTaskListByDate(item) {
+    // todo
   }
 
 }
